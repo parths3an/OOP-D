68b23476cd2e57e87cf3417362fbcf4cb756ce3f
Parth M Shah <pms002@ieng6-202.ucsd.edu>
2017-12-19 17:23:18 -0800

    Overloaded the insertation operator.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
 Name_pairs np1; 
+Name_pairs np2; 
 
 string inString;
 double inAge;
@@ -24,22 +25,30 @@ while( inputVal != 'n')
 
    //Inserting the data to the inPairVec;
    inPairVec.push_back(make_pair(inString,inAge)); 
+   inputVal ='n';	
 }
 cout << "Reading data from the input..." << endl;
 //Inserting into the member vec of the class 
 np1.readPair(inPairVec);
+np2.readPair(inPairVec);
 
 //printing the vector
 cout << "Printing data from the nameAgePairVec... \n";
 np1.print();
+np2.print();
 
 //Printing from the individual vector
 cout << "Printing data from individual vectors... \n";
 np1.printFromVec();
+np2.printFromVec();
 
 cout << "Sorting the vector of pairs and printing after sorting...\n";
 np1.sortPairs();
 np1.print();
+np2.sortPairs();
+np2.print();
 
+cout << "Checking insertation operator" << endl;
+cout << np1;
 return 0;
 }
