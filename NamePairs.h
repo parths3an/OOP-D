346754755ef56c3cec49b852345344e9cc712196
Parth Shah <kingcobra@Parths-MacBook-Pro.local>
2017-12-19 16:44:41 -0800

    overloaded the insertation operator

--- NamePairs.h
+++ NamePairs.h
@@ -28,10 +28,29 @@ bool myComparison(pair<string,double> i,pair<string,double> j)
 
   return (i.first > j.first);
 }
+
+//Operator overloading, insertation operator
+ostream& operator<<(ostream& os, const Name_pairs& npObj)
+{
+    string literals = " ";
+    string newLine= "\n";
+    
+     for(auto a : npObj.nameAgePairVec )
+    {
+     double val = a.second;
+     os << a.first << literals << to_string(val) << newLine;
+    }
+    return os;
+}
+
+
+
 private:
 vector< pair<string,double> >  nameAgePairVec;
 vector <double> ageVec;
 vector <string> nameVec;
+
+
 };
 
 #endif 
